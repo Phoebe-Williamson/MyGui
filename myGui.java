d38fbe0f30d520a4d26fab8d38263c9683aed9c4
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2023-03-09 14:44:15 +1300

    tbh i dont know what i did here

--- myGui.java
+++ myGui.java
@@ -10,6 +10,9 @@ public class myGui
 {
     // instance variables - replace the example below with your own
     private double speed;
+    
+    // fileds to remember the pressed position
+    private double startX, startY; 
 
     /**
      * Constructor for objects of class myGui
@@ -21,9 +24,14 @@ public class myGui
         
         // set up buttons
         UI.addButton("Quit", UI::quit);
+        UI.addButton("Clear", this::doClear);
         
         // setup slider
         UI.addSlider("Speed", 0, 100, 20, this::setSpeed);
+        
+        // setup mouse listener
+        UI.setLineWidth(10);
+        UI.setMouseListener(this::doMouse);
     }
     
     /**
@@ -38,5 +46,28 @@ public class myGui
         } else {
             UI.println("Stationary");
         }
+    }
+    
+    /**
+     * call back method to mouse responder
+     * only make one call back method to the mouse listener
+     */
+    public void doMouse(String action, double x, double y) {
+        if (action.equals("pressed")) {
+         this.startX = x;
+         this.startY = y;
+        } else if (action.equals("released")){
+            UI.drawLine(this.startX, this.startY, x, y);
+        }
+    }
+    
+    /**
+     * make the screen clear
+     * 
+     */
+    public void doClear() {
+        UI.setColor(Color.white);
+        UI.fillRect(0, 0, 100000, 100000);
+        
     }
 }
